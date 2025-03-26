83a2a232cc63b844a9dd1dbd7a921beb624d536a
Temur Coder <temurnarzullayev810@gmail.com>
2025-03-26 15:52:32 +0300

    added

---
+++ fetch/script.js
@@ -0,0 +1,26 @@
+'use strict';
+const API = 'https://reqres.in/api/users';
+let createUserBtn = document.querySelector('#createBtn');
+let saveContainer = document.querySelector('#saveContainer');
+let message = document.querySelector('.message')
+let user = [];
+
+const createUser = async () => {
+  const name = document.getElementById("name").value;
+  const job = document.getElementById("job").value;
+  if (!name || !job) {
+    message.textContent = "Iltimos ismingiz bilan kasbingizni kiriting";
+  }
+  try {
+    const response = await fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, job }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    message.textContent = "Iltimos ismingiz bilan kasbingizni kiriting";
+  }
+};
+createUserBtn.addEventListener("click", createUser);
